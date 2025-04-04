f13c854aa3f4d8bef7163276305053a48860f414
Qiqi Jin <ginqi7@gmail.com>
2025-04-04 17:08:36 +0800

    feat: Utilize the URL object to manage Bilibili links.

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ async function replaceBilibiliUrls(html) {
     const links = $('a[href*="https://www.bilibili.com/video/"]');
     // Add Styles in the html.
     if (links.length > 0) {
-        console.log(BILIBILI_CARD_STYEL);
         const customStyles = fs.readFileSync(BILIBILI_CARD_STYEL);
         if ($("style").text().indexOf(customStyles) === -1) {
             $("head").append(`<style>${customStyles}</style>`);
@@ -24,12 +23,14 @@ async function replaceBilibiliUrls(html) {
     for (let i = 0; i < links.length; i++) {
         const link = links[i];
         const href = $(link).attr("href");
-        console.log(href);
-        // Extract bilibili type and ID
-        const match = href.match(/https:\/\/www\.bilibili\.com\/video\/(.*)/);
-        if (match && match[1]) {
-            const bilibiliId = match[1];
-            const rest = await createCard(imageProxy, bilibiliId);
+        const url = new URL(href);
+        const pathParts = url.pathname.split("/").filter(Boolean); // 分割路径并过滤空字符串
+        const bilibiliId = pathParts[pathParts.length - 1];
+        const rest = await createCard(imageProxy, bilibiliId);
+        const errorStr = '<p class="title">出错了！</p>';
+        if (rest.includes(errorStr)) {
+            console.log(`Error: parse for ${href}`);
+        } else {
             // Replace the URL with the rendering result
             $(link).replaceWith(rest);
         }
