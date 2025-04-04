03d2661bbd9cd02f791fce61e8fa766e99f76f87
Qiqi Jin <ginqi7@gmail.com>
2025-04-04 16:51:02 +0800

    This package provides a general HTML handler for Bilibili links.

--- README.md
+++ README.md
@@ -1,53 +1,11 @@
-# hexo-bilibili-card
+## HTML-BILIBILI-CARD
 
-A hexo plugin insert a bilibili card into your page or article, some of css code is copy from bilibili.
+It is a fork of https://github.com/maxchang3/hexo-bilibili-card, which was originally designed for Hexo. This package provides a general HTML handler for Bilibili links.
 
-一个Hexo插件，在你的文章中插入b站的视频卡片，样式模仿和借鉴自b站。
+### Usage
 
-[![NPM](https://nodei.co/npm/hexo-bilibili-card.png)](https://nodei.co/npm/hexo-bilibili-card/)
+First, please install the plugin.
 
-> 目前此插件处于非积极维护中，可能需要修改代码样式以适配自己的网站。
-
-> 此插件样式可能会受到站内样式污染，后期会改为 SVG 生成或 Web Component。短时间内获取大量图片信息可能会受到哔哩哔哩的访问限制。
-
-> This plugin's CSS styles may be contaminated by the site's CSS styles, which will be changed to SVG generation at a later date. Fetching a large number of images/info over a period of time may be subject to access restrictions.
-
-
-## Install 安装
-
-```npm i hexo-bilibili-card```
-
-## Preview 预览
-
-You can click [here](https://zhangmaimai.com/2021/02/05/some-tests/) to see the preview website.
-
-点击 [这里](https://zhangmaimai.com/2021/02/05/some-tests/) 看预览哦~
-
-## Usage 使用
-
-In your config file, insert follow codes:
-
-在你的 config 文件中插入以下片段
-
-```
-{% bilicard your_video_id %}
-```
-`your_video_id` is the avid or bvid in Bilibili.
-
-`your_video_id` 是b站的bv号或av号。
-
-then you will get the card in your page.
-
-然后你就可以看见文章中的卡片了。
-
-
-## Config 配置
-
-由于 b 站图片地址的跨域限制，因此需要配置图片代理，目前未找到大陆地区较为稳定的服务，默认使用的 `https://images.weserv.nl/?url=`，部分地区受到了 DNS 污染及反应过慢。建议参考 [rsstt-img-relay](https://github.com/Rongronggg9/rsstt-img-relay) 自建服务。
-
-Due to the CORS strategy of Bilibili, you need to configure an image proxy to get the image correctly. Unfortunately, we haven’t found any stable image proxy in China Mainland yet. The default image proxy is provided by `https://images.weserv.nl/?url=`, which has DNS pollution in some areas and the response is too slow. We suggest referring to [rsstt-img-relay](https://github.com/Rongronggg9/rsstt-img-relay) to build your image proxy instead.
-
-```yaml
-hexo_bilibili_card:
-    imageProxy: https://images.weserv.nl/?url=
+```bash
+$ npm install html-bilibili-card --save
 ```
--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-'use strict'
-const fs = require('hexo-fs')
-const path = require('path')
-const css = hexo.extend.helper.get('css').bind(hexo)
-const { createCard } = require('./lib/bili.js')
-const get = require('lodash.get')
+"use strict";
+const fs = require("hexo-fs");
+const path = require("path");
+const { createCard } = require("./lib/bili.js");
+const get = require("lodash.get");
+const imageProxy = "https://images.weserv.nl/?url=";
+const cheerio = require("cheerio");
 
-const { config } = hexo
+const BILIBILI_CARD_STYEL = path.resolve(__dirname, "./source/bilicard.css");
 
-const imageProxy = get(config, 'hexo_bilibili_card.imageProxy', "https://images.weserv.nl/?url=")
-
-hexo.extend.generator.register('bili_asset', () => [{
-  path: 'css/bilicard.css',
-  data: () => fs.createReadStream(path.resolve(__dirname, "./source", "bilicard.css"))
+async function replaceBilibiliUrls(html) {
+    const $ = cheerio.load(html); // Load HTML
+    // Query all <a> tags containing bilibili URL
+    const links = $('a[href*="https://www.bilibili.com/video/"]');
+    // Add Styles in the html.
+    if (links.length > 0) {
+        console.log(BILIBILI_CARD_STYEL);
+        const customStyles = fs.readFileSync(BILIBILI_CARD_STYEL);
+        if ($("style").text().indexOf(customStyles) === -1) {
+            $("head").append(`<style>${customStyles}</style>`);
+        }
+    }
+    // Iterate through each link
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        const href = $(link).attr("href");
+        console.log(href);
+        // Extract bilibili type and ID
+        const match = href.match(/https:\/\/www\.bilibili\.com\/video\/(.*)/);
+        if (match && match[1]) {
+            const bilibiliId = match[1];
+            const rest = await createCard(imageProxy, bilibiliId);
+            // Replace the URL with the rendering result
+            $(link).replaceWith(rest);
+        }
+    }
+    return $.html();
 }
-])
 
-hexo.extend.tag.register("bilicard", async (args) => {
-  return await createCard(imageProxy, args[0])
-}, { async: true })
-
-hexo.extend.injector.register('head_begin', () => {
-  return css('/css/bilicard.css')
-}, 'post')
+module.exports = replaceBilibiliUrls;
